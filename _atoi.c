e0aebc5675c9df7b1f1a82bb78443b2c6aca6939
gooddids22 <splendidotamiri@gmail.com>
2023-05-17 10:43:26 +0100

    builtin.c

--- _atoi.c
+++ _atoi.c
@@ -2,11 +2,10 @@
 #include <stdio.h>
 
 /**
- * interactive - returns true if shell is interactive mode
- * @info: struct address
- * 
- * Return: 1 if interactive mode, 0 otherwise
- */
+* interactive - returns true if shell is interactive mode
+* @info: struct address
+* Return: 1 if interactive mode, 0 otherwise
+*/
 
 int interactive(info_t *info)
 {
